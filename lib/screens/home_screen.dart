865f522a862ff87389d49f352c7fb5fc52ebd973
shilpa-dogra-gif <shilpa_dogra@outlook.com>
2025-07-14 23:31:39 +0530

    Add lib/screens/home_screen.dart 1752516094949

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('My Collages')),
+    Center(child: Text('Create New')),
+    Center(child: Text('Themes')),
+    Center(child: Text('Settings')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Baby Memory Collage'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_album),
+            label: 'My Collages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Create New',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.palette),
+            label: 'Themes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.pink,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
